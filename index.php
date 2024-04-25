cd09eaef0918dbbd395b1688f71696dae6667b5e
Evgeny Kampiler <kampiler@gmail.com>
2024-04-26 02:20:40 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=100%, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello World from P2p
+    <?php
+    echo $_SERVER['REMOTE_ADDR'];
+    phpinfo();
+    
+    ?>
+</body>
+</html>
