9ff4d9b32c790d2fc73feae3cecd32aa0cb2c060
Evgeny Kampiler <kampiler@gmail.com>
2024-04-26 02:48:05 +0300

    11

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=100%, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    Hello World from P2p
+    Здравствуй МИР!!! <hr>
+
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+
+    <hr>
     <?php
-    echo $_SERVER['REMOTE_ADDR'];
+    echo $_SERVER['REMOTE_ADDR']."<hr>\n";
     phpinfo();
-    
+    #
     ?>
 </body>
 </html>
